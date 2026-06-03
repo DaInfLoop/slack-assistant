047e4832af5adb9835b06e51168d0340255c4e5e
DaInfLoop <github@dainfloop.is-a.dev>
2026-06-03 08:06:25 +0100

    Add Postgres helper functions

--- postgres.ts
+++ postgres.ts
@@ -21,3 +21,31 @@ export async function setupTables() {
         )`
     ])
 }
+
+export async function getUserLLAC(userId: string): Promise<string | null> {
+    const res = await sql<{ llac: string }[]>`SELECT llac FROM access_tokens WHERE user_id = ${userId}`;
+
+    if (res.length === 0) return null;
+    else return res[0]!.llac
+}
+
+export async function setUserLLAC(userId: string, llac: string): Promise<void> {
+    await sql`INSERT INTO access_tokens(user_id, llac)
+    VALUES (${userId}, ${llac})
+    ON CONFLICT (user_id)
+    DO UPDATE SET llac = EXCLUDED.llac`
+}
+
+export async function getThreadMapping(threadId: string): Promise<string | null> {
+    const res = await sql<{ conversation_id: string }[]>`SELECT conversation_id FROM thread_mappings WHERE thread_id = ${threadId}`;
+
+    if (res.length === 0) return null;
+    else return res[0]!.conversation_id
+}
+
+export async function setThreadMapping(threadId: string, conversationId: string): Promise<void> {
+    await sql`INSERT INTO thread_mappings(thread_id, conversation_id)
+    VALUES (${threadId}, ${conversationId})
+    ON CONFLICT (thread_id)
+    DO NOTHING`
+}
